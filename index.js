6160d23794bf4ae40fdbfd7ce7afcbf5b56ee348
Yomex1996 <abayomirobertonawole@gmail.com>
2026-03-10 17:54:45 +0100

    okaywsyt

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const PORT = process.env.PORT || 3000;
 
 const host = process.env.DB_HOST || 'localhost'; 
 
-
-
-
-
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
