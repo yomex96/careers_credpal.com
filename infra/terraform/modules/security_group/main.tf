6e3a28c5325d26fe8ccd8da2ae6af5c659af28d5
Abayomi Robert Onawole <142827967+yomex96@users.noreply.github.com>
2026-03-11 12:05:13 +0100

    Add files via upload

---
+++ infra/terraform/modules/security_group/main.tf
@@ -0,0 +1,44 @@
+resource "aws_security_group" "alb_sg" {
+  name   = "${var.app_name}-alb-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name   = "${var.app_name}-ec2-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
